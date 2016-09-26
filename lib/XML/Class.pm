8cfc7cef5fccdeb04b7d5cf1c3bb8d02875c28ae
Jonathan Stowe <jns@gellyfish.co.uk>
2016-09-26 17:56:55 +0100

    Except that's a sub not a method

--- lib/XML/Class.pm
+++ lib/XML/Class.pm
@@ -1151,7 +1151,7 @@ role XML::Class[Str :$xml-namespace, Str :$xml-namespace-prefix, Str :$xml-eleme
     }
 
     multi sub create-element(Str:D $name, Str $xml-namespace, $xml-namespace-prefix?) {
-        my $xe = self.create-element($name);
+        my $xe = create-element($name);
         if $xml-namespace.defined {
             $xe.setNamespace($xml-namespace, $xml-namespace-prefix);
         }
